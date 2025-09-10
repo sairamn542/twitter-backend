411ebfc1a1fa3613ba1f749430fd0f539a538b62
sairamn542 <sairamn542@gmail.com>
2025-09-10 22:19:11 +0530

    first

--- index.js
+++ index.js
@@ -18,15 +18,28 @@ app.use(express.urlencoded({
 app.use(express.json())
 app.use(cookieParser())
 
-const corsOption = {
-  origin: [
-    "http://localhost:3000",
-    "https://twitter-sairam.netlify.app"
-  ],
-  credentials: true,
+// const corsOption = {
+//   origin: "http://localhost:3000",
+//   credentials: true,
+// };
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://twitter-sairam.netlify.app"
+];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // allow requests like Postman
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = "CORS Error: Origin not allowed";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true // required if using cookies
 };
-app.use(cors(corsOption))
-app.use(cors(corsOption))
+
+app.use(cors(corsOptions))
 //api
 app.use("/api/v1/user", userRoute)
 app.use("/api/v1/tweet", tweetRoute)
